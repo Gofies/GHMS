eb5fc9df84d40fb8080c6226e7e8854c9012cf1e
AbdullahSh20 <abdullah.shammout@hotmail.com>
2024-10-20 17:44:12 +0300

    bug: service names

--- healthycheck.sh
+++ healthycheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-services=( "backend" "frontend" "redis" "mongo" "postgres" )
+services=( "ghms_backend_1" "ghms_frontend_1" "ghms_redis_1" "ghms_mongo_1" "ghms_postgres_1" )
 for service in "${services[@]}"; do
   echo "Checking health of $service..."
   until [ "`docker inspect -f {{.State.Health.Status}} $service`" == "healthy" ]; do
