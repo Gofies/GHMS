2cc602278652b954314ced0569af847aabaa5b48
AbdullahSh20 <abdullah.shammout@hotmail.com>
2024-10-20 17:41:33 +0300

    bug: add health check to compose

--- healthycheck.sh
+++ healthycheck.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-
-services=( "ghms_backend_1" "ghms_frontend_1" "ghms_redis_1" "ghms_mongo_1" "ghms_postgres_1" )
+services=( "backend" "frontend" "redis" "mongo" "postgres" )
 for service in "${services[@]}"; do
-printf "Checking health of %s...\n" "$service"
-until [ "`docker inspect -f {{.State.Health.Status}} $service`" == "healthy" ]; do
-    printf "%s is not healthy yet. Waiting...\n" "$service"
+  echo "Checking health of $service..."
+  until [ "`docker inspect -f {{.State.Health.Status}} $service`" == "healthy" ]; do
+    echo "$service is not healthy yet. Waiting..."
     sleep 5
-done
-printf "%s is healthy!\n" "$service"
+  done
+  echo "$service is healthy!"
 done
