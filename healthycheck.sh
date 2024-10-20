bc07adeb1f221f982c78d84bd4020392fcdc1406
AbdullahSh20 <abdullah.shammout@hotmail.com>
2024-10-20 17:35:30 +0300

    bug: fixed script

--- healthycheck.sh
+++ healthycheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-services=( "backend" "frontend" "postgres" "mongo" "redis" )
+services=( "ghms_backend_1" "ghms_frontend_1" "ghms_redis_1" "ghms_mongo_1" "ghms_postgres_1" )
 for service in "${services[@]}"; do
 printf "Checking health of %s...\n" "$service"
 until [ "`docker inspect -f {{.State.Health.Status}} $service`" == "healthy" ]; do
