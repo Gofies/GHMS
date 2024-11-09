b18352ca5bf3d7c98197180a4e244cd8bb221a9e
Berkay Gürsu <berkay.gursu@codevo.com.tr>
2024-11-09 21:29:49 +0300

    feat: patient - appointments page is implemented

---
+++ frontend/src/components/ui/patient/appointment/Dialog.jsx
@@ -0,0 +1,24 @@
+// Dialog.js
+import React, { useState } from 'react';
+
+export const Dialog = ({ children }) => <div>{children}</div>;
+
+export const DialogTrigger = ({ asChild, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const child = React.cloneElement(children, { onClick: () => setIsOpen(!isOpen) });
+  return <>{child}</>;
+};
+
+export const DialogContent = ({ children }) => (
+  <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
+    <div className="bg-white rounded-lg p-6 max-w-lg w-full">{children}</div>
+  </div>
+);
+
+export const DialogHeader = ({ children }) => (
+  <div className="mb-4">{children}</div>
+);
+
+export const DialogTitle = ({ children }) => (
+  <h2 className="text-xl font-bold">{children}</h2>
+);
--- frontend/src/views/app/patient/index.js
+++ frontend/src/views/app/patient/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import PatientHomePage from "../../../pages/patient/PatientHomePage.tsx";
+import PatientAppointmentsPage from "../../../pages/patient/PatientAppointmentsPage.tsx";
 
 const PatientView = () => {
   return (
     <Routes>
       <Route path="/" element={<PatientHomePage />} />
+      <Route path="/appointments" element={<PatientAppointmentsPage />} />
+
       {/* Add additional routes here for PatientView */}
       {/* Example: <Route path="/settings" element={<PatientSettings />} /> */}
    </Routes>
