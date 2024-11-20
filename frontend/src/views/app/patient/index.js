b3b09d6883dd7f2a29e4f76dad878d75c715e146
itu-itis21-oztorun20 <92295752+itu-itis21-oztorun20@users.noreply.github.com>
2024-11-20 16:18:36 +0300

    feat: patient medical records page implemented (#67)
    Co-authored-by: Abdullah Shamout <117589058+AbdullahSh20@users.noreply.github.com>

--- frontend/src/views/app/patient/index.js
+++ frontend/src/views/app/patient/index.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import PatientHomePage from "../../../pages/patient/PatientHomePage.tsx";
 import PatientHealthMetricsPage from "../../../pages/patient/PatientHealthMetricsPage.tsx";
 import PatientAppointmentsPage from "../../../pages/patient/PatientAppointmentsPage.tsx";
+import PatientMedicalRecordsPage from "../../../pages/patient/PatientMedicalRecordsPage.tsx";
 import PatientSettingsPage from "../../../pages/patient/PatientSettingsPage.tsx";
 
 const PatientView = () => {
@@ -11,6 +12,7 @@ const PatientView = () => {
       <Route path="/" element={<PatientHomePage />} />
       <Route path="/health-metrics" element={<PatientHealthMetricsPage />} />
       <Route path="/appointments" element={<PatientAppointmentsPage />} />
+      <Route path="/medical-records" element={<PatientMedicalRecordsPage />} />
       <Route path="/settings" element={<PatientSettingsPage />} />
       {/* Add additional routes here for PatientView */}
       {/* Example: <Route path="/settings" element={<PatientSettings />} /> */}
