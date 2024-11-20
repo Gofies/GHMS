d4580af9ba743b75a7b902f7e11a610e540737f8
itu-itis21-oztorun20 <92295752+itu-itis21-oztorun20@users.noreply.github.com>
2024-11-20 16:14:57 +0300

    Feature/patient settings page implementation (#65)
    * feat: patient medical records page implemented
    
    * feat: setting page implementation/dark mode...
    
    * feat: patient settings page implementation
    
    ---------
    
    Co-authored-by: Abdullah Shamout <117589058+AbdullahSh20@users.noreply.github.com>

--- frontend/src/views/app/patient/index.js
+++ frontend/src/views/app/patient/index.js
@@ -3,12 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import PatientHomePage from "../../../pages/patient/PatientHomePage.tsx";
 import PatientHealthMetricsPage from "../../../pages/patient/PatientHealthMetricsPage.tsx";
 import PatientAppointmentsPage from "../../../pages/patient/PatientAppointmentsPage.tsx";
+import PatientSettingsPage from "../../../pages/patient/PatientSettingsPage.tsx";
+
 const PatientView = () => {
   return (
     <Routes>
       <Route path="/" element={<PatientHomePage />} />
       <Route path="/health-metrics" element={<PatientHealthMetricsPage />} />
       <Route path="/appointments" element={<PatientAppointmentsPage />} />
+      <Route path="/settings" element={<PatientSettingsPage />} />
       {/* Add additional routes here for PatientView */}
       {/* Example: <Route path="/settings" element={<PatientSettings />} /> */}
    </Routes>
