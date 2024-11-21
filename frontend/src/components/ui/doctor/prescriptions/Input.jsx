7d18d19eba48f1550d4ed8dd282561bad1aebe71
itu-itis20-oztorun20 <oztorun20@itu.edu.tr>
2024-11-21 16:53:55 +0300

    feat: malım

--- frontend/src/components/ui/doctor/prescriptions/Input.jsx
+++
@@ -1,9 +0,0 @@
-export function Input({ className, ...props }) {
-    return (
-      <input
-        className={`w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 ${className}`}
-        {...props}
-      />
-    )
-  }
-  
