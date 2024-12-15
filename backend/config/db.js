a28b85aa4ece1d2a3c72e8452cd1434539464792
canetizen <mcancaliskan@outlook.com>
2024-12-15 16:59:09 +0300

    healthcheck trial

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,8 +7,8 @@ dotenv.config();
 const connectToMongoDB = async () => {
     try {
         const mongoURI = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@router01:27117,router02:27118/HospitalDatabase?authMechanism=DEFAULT&authSource=admin`;
-    
-        await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+        // const mongoURI = process.env.TEST_URI_1
+        await mongoose.connect(mongoURI);
     
         console.log("Connected to the MongoDB database");
     } catch (error) {
