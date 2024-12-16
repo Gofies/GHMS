c36c1206bfa24cf4be9838f094eb083d360f95d9
Can Çalışkan <81326097+canetizen@users.noreply.github.com>
2024-12-16 13:43:47 +0300

    ci trial 1 (#106)
    * ci trial 1
    
    * trial 2
    
    * verbosity increased trial
    
    * docker-compose correction
    
    * last trial i hope
    
    * forgot to -d

--- database/init.sh
+++ database/init.sh
@@ -51,16 +51,16 @@ execute_with_retry() {
 }
 
 commands=(
-    "docker-compose exec configsvr01 sh -c 'mongosh --port 27119 < /scripts/init-configserver.js'"
-    "docker-compose exec shard01-a sh -c 'mongosh --port 27122 < /scripts/init-shard01.js'"
-    "docker-compose exec shard02-a sh -c 'mongosh --port 27125 < /scripts/init-shard02.js'"
-    "docker-compose exec shard03-a sh -c 'mongosh --port 27128 < /scripts/init-shard03.js'"
-    "docker-compose exec router01 sh -c 'mongosh --port 27117 < /scripts/init-router.js'"
-    "docker-compose exec configsvr01 sh -c 'mongosh admin --port 27119 --eval \"db.createUser({user: \\\"$MONGO_USERNAME\\\", pwd: \\\"$MONGO_PASSWORD\\\", roles:[{role: \\\"root\\\", db: \\\"admin\\\"}]}); db.auth(\\\"$MONGO_USERNAME\\\", \\\"$MONGO_PASSWORD\\\");\"'"
-    "docker-compose exec shard01-a sh -c 'mongosh admin --port 27122 --eval \"db.createUser({user: \\\"$MONGO_USERNAME\\\", pwd: \\\"$MONGO_PASSWORD\\\", roles:[{role: \\\"root\\\", db: \\\"admin\\\"}]}); db.auth(\\\"$MONGO_USERNAME\\\", \\\"$MONGO_PASSWORD\\\");\"'"
-    "docker-compose exec shard02-a sh -c 'mongosh admin --port 27125 --eval \"db.createUser({user: \\\"$MONGO_USERNAME\\\", pwd: \\\"$MONGO_PASSWORD\\\", roles:[{role: \\\"root\\\", db: \\\"admin\\\"}]}); db.auth(\\\"$MONGO_USERNAME\\\", \\\"$MONGO_PASSWORD\\\");\"'"
-    "docker-compose exec shard03-a sh -c 'mongosh admin --port 27128 --eval \"db.createUser({user: \\\"$MONGO_USERNAME\\\", pwd: \\\"$MONGO_PASSWORD\\\", roles:[{role: \\\"root\\\", db: \\\"admin\\\"}]}); db.auth(\\\"$MONGO_USERNAME\\\", \\\"$MONGO_PASSWORD\\\");\"'"
-    "docker-compose exec router01 mongosh --port 27117 -u \"$MONGO_USERNAME\" -p \"$MONGO_PASSWORD\" --authenticationDatabase admin --eval \"sh.enableSharding('HospitalDatabase')\""
+    "docker exec mongo-config-01 sh -c 'mongosh --port 27119 < /scripts/init-configserver.js'"
+    "docker exec shard-01-node-a sh -c 'mongosh --port 27122 < /scripts/init-shard01.js'"
+    "docker exec shard-02-node-a sh -c 'mongosh --port 27125 < /scripts/init-shard02.js'"
+    "docker exec shard-03-node-a sh -c 'mongosh --port 27128 < /scripts/init-shard03.js'"
+    "docker exec router-01 sh -c 'mongosh --port 27117 < /scripts/init-router.js'"
+    "docker exec mongo-config-01 sh -c 'mongosh admin --port 27119 --eval \"db.createUser({user: \\\"$MONGO_USERNAME\\\", pwd: \\\"$MONGO_PASSWORD\\\", roles:[{role: \\\"root\\\", db: \\\"admin\\\"}]}); db.auth(\\\"$MONGO_USERNAME\\\", \\\"$MONGO_PASSWORD\\\");\"'"
+    "docker exec shard-01-node-a sh -c 'mongosh admin --port 27122 --eval \"db.createUser({user: \\\"$MONGO_USERNAME\\\", pwd: \\\"$MONGO_PASSWORD\\\", roles:[{role: \\\"root\\\", db: \\\"admin\\\"}]}); db.auth(\\\"$MONGO_USERNAME\\\", \\\"$MONGO_PASSWORD\\\");\"'"
+    "docker exec shard-02-node-a sh -c 'mongosh admin --port 27125 --eval \"db.createUser({user: \\\"$MONGO_USERNAME\\\", pwd: \\\"$MONGO_PASSWORD\\\", roles:[{role: \\\"root\\\", db: \\\"admin\\\"}]}); db.auth(\\\"$MONGO_USERNAME\\\", \\\"$MONGO_PASSWORD\\\");\"'"
+    "docker exec shard-03-node-a sh -c 'mongosh admin --port 27128 --eval \"db.createUser({user: \\\"$MONGO_USERNAME\\\", pwd: \\\"$MONGO_PASSWORD\\\", roles:[{role: \\\"root\\\", db: \\\"admin\\\"}]}); db.auth(\\\"$MONGO_USERNAME\\\", \\\"$MONGO_PASSWORD\\\");\"'"
+    "docker exec router-01 mongosh --port 27117 -u \"$MONGO_USERNAME\" -p \"$MONGO_PASSWORD\" --authenticationDatabase admin --eval \"sh.enableSharding('HospitalDatabase')\""
 )
 
 # Execute each command with retries
@@ -71,7 +71,7 @@ done
 collections=("admins" "appointments" "diagnoses" "doctors" "hospitals" "labtechnicians" "labtests" "patients" "polyclinics" "prescriptions" "treatments")
 for collection in "${collections[@]}"; do
     log "Sharding the collection: $collection..."
-    execute_with_retry "docker-compose exec router01 mongosh --port 27117 -u \"${MONGO_USERNAME}\" -p \"${MONGO_PASSWORD}\" --authenticationDatabase admin --eval 'db.adminCommand({ shardCollection: \"HospitalDatabase.${collection}\", key: { _id: \"hashed\" } })'" # random
+    execute_with_retry "docker exec router-01 mongosh --port 27117 -u \"${MONGO_USERNAME}\" -p \"${MONGO_PASSWORD}\" --authenticationDatabase admin --eval 'db.adminCommand({ shardCollection: \"HospitalDatabase.${collection}\", key: { _id: \"hashed\" } })'" # random
 done
 
 docker exec -it router-01 bash -c "echo 'sh.status()' | mongosh --port 27117 -u \"${MONGO_USERNAME}\" -p \"${MONGO_PASSWORD}\" --authenticationDatabase admin"
@@ -88,7 +88,7 @@ for json_file in "${json_files[@]}"; do
     
     collection_name=$(basename "$json_file" .json)
     
-    docker-compose exec router01 sh -c "mongoimport --host router01 --port 27117 --db HospitalDatabase --collection $collection_name --file /data/$json_file --jsonArray --username $MONGO_USERNAME --password $MONGO_PASSWORD --authenticationDatabase admin"
+    docker exec router-01 sh -c "mongoimport --host router-01 --port 27117 --db HospitalDatabase --collection $collection_name --file /data/$json_file --jsonArray --username $MONGO_USERNAME --password $MONGO_PASSWORD --authenticationDatabase admin"
 done
 
 
