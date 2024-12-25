271ae1def415c7f3ee8381e9f8ceeaedaadf69d3
AbdullahSh20 <abdullah.shammout@hotmail.com>
2024-12-25 11:47:34 +0300

    bugfix

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,9 @@ dotenv.config();
 // Connect to MongoDB
 const connectToMongoDB = async () => {
     try {
-        //const mongoURI = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@router01:27117,router02:27118/HospitalDatabase?authMechanism=DEFAULT&authSource=admin`;
-        const mongoURI = process.env.MONGODB_URI;
-        await mongoose.connect(mongoURI);
+        const mongoURI = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@router01:27117,router02:27118/HospitalDatabase?authMechanism=DEFAULT&authSource=admin`;
+
+        await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
         console.log("Connected to the MongoDB database");
     } catch (error) {
