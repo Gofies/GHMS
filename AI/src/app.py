290dcc086348b3207b1dd271a35dd33981085d8f
AbdullahSh20 <abdullah.shammout@hotmail.com>
2024-12-25 18:57:50 +0300

    prometheus is fully connected and functional

--- AI/src/app.py
+++ AI/src/app.py
@@ -1,12 +1,17 @@
-# src/app.py
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from routes import register_routes
 from services import FileService
+import time
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    
+    # Track request time
+    @app.before_request
+    def start_timer():
+        request.start_time = time.time()
     
     # Initialize services
     file_service = FileService(app.config['UPLOAD_FOLDER'])
